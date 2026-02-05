2ed1f8940890df3e7154b70d7d7947c1a0626caa
Cuauhtemoc Herrera <cuauhtemoc.herrera@oracle.com>
2026-02-04 20:11:36 -0600

    springboot starter files and heroku config

---
+++ src/main/java/com/seifersonlabs/rummyscore/RummyscoreApplication.java
@@ -0,0 +1,13 @@
+package com.seifersonlabs.rummyscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RummyscoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RummyscoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seifersonlabs/rummyscore/RummyscoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seifersonlabs.rummyscore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RummyscoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
