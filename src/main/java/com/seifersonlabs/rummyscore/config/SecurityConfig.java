c1789fe27535dab803220a7053bb331ad5fb5cfd
Cuauhtemoc Herrera <cuauhtemoc.herrera@oracle.com>
2026-02-13 16:27:05 -0600

    fixed withdefaults to use oauth2 object

--- src/main/java/com/seifersonlabs/rummyscore/config/SecurityConfig.java
+++ src/main/java/com/seifersonlabs/rummyscore/config/SecurityConfig.java
@@ -5,8 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 @Configuration
 public class SecurityConfig {
 
@@ -17,7 +15,9 @@ public class SecurityConfig {
                         .requestMatchers("/", "/api/v1/check").permitAll()
                         .anyRequest().authenticated()
                 )
-                .oauth2Login(withDefaults());;
+                .oauth2Login(oauth2 -> oauth2
+                        .defaultSuccessUrl("/", true)
+                );
 
         return http.build();
     }
