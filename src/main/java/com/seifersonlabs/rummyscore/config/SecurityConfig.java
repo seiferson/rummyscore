3a280246d66afd65642e694f1b92f0047aa7112d
Cuauhtemoc Herrera <cuauhtemoc.herrera@oracle.com>
2026-02-13 16:07:24 -0600

    added oauth2login on filterchain

--- src/main/java/com/seifersonlabs/rummyscore/config/SecurityConfig.java
+++ src/main/java/com/seifersonlabs/rummyscore/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 public class SecurityConfig {
 
@@ -14,7 +16,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers("/").permitAll()
                         .anyRequest().authenticated()
-                );
+                )
+                .oauth2Login(withDefaults());;
 
         return http.build();
     }
