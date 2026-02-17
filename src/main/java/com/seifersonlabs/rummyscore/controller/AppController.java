786f2776ab118a7ed957321a07266abe2e3b35b7
Cuauhtemoc Herrera <cuauhtemoc.herrera@oracle.com>
2026-02-16 20:52:49 -0600

    created very basic match info page

---
+++ src/main/java/com/seifersonlabs/rummyscore/controller/AppController.java
@@ -0,0 +1,15 @@
+package com.seifersonlabs.rummyscore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class AppController {
+
+    @GetMapping("/matches/{matchId}")
+    public String matches(@PathVariable String matchId) {
+        return "match";
+    }
+}
